592555b44f8a90524250524ead66c97b51b8950e
Sakaria Abukar <148639438+SAA1960@users.noreply.github.com>
2024-09-03 21:11:41 +0200

    Create analysis.py

---
+++ analysis.py
@@ -0,0 +1,17 @@
+# Python program to round a float value to two decimal places  
+  
+# creating a floating-point value  
+number = 3.3469456  
+# rounding the above number   
+rounded_number = round(number, 2)  
+# printing the result  
+print("Rounding 3.3469456 up to two decimal places:",  rounded_number)  
+
+# Python program to round a float value to two decimal places  
+  
+# creating a floating-point value  
+number = 3.3469456  
+# rounding the above number   
+rounded_number = round(number, 2)  
+# printing the result  
+print("Rounding 3.3469456 up to two decimal places:",  rounded_number)  
